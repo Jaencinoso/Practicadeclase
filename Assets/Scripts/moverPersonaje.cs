0f43633f66e0d92071007cca5c8badea2d6bc01f
Jaencinoso <jaencinoso@gmail.com>
2023-10-03 16:06:58 +0100

    Día 2
    hoy he terminado de dar la obtención de  coleccionables, pero no su utilidad todavía, y tambien añadí los "Giroscopios"

--- Assets/Scripts/moverPersonaje.cs
+++ Assets/Scripts/moverPersonaje.cs
@@ -7,6 +7,7 @@ public class moverPersonaje : MonoBehaviour
     public float movX, movY;
     public float fuerzaSalto = 4;
     public bool quiereSaltar = false;
+    public bool estaSuelo = false;
     Rigidbody2D fisicas;
     // Start is called before the first frame update
     void Start()
@@ -29,10 +30,31 @@ public class moverPersonaje : MonoBehaviour
         Vector2 movimiento = new Vector2(movX * 2, fisicas.velocity.y);
         fisicas.velocity = movimiento;
 
-        if (quiereSaltar)
+        if (quiereSaltar && estaSuelo)
         {
             fisicas.AddForce( Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
             quiereSaltar = false;
+            estaSuelo = false;
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Suelo")
+        {
+            estaSuelo |= true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Monedas")
+        {
+            Destroy(collision.gameObject);
+        }
+        else if(collision.gameObject.tag == "Estrellas")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    
+
 }
